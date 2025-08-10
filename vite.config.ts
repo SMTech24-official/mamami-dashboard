26abc2f52a1f9b2b8df5b9dcfe0666c4486d2b99
aburayhan-bpi <aburayhan.bpi@gmail.com>
2025-08-10 16:44:19 +0600

    updated vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,21 @@
 //   plugins: [react(), tailwindcss()],
 // });
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000, // Set development server port to 3000
-    strictPort: true, // Exit if port 3000 is already in use
-    open: true // Optional: automatically open browser
+    host: "0.0.0.0", // to bind to all network interfaces
+    allowedHosts: [
+      "vybly.io", // Allow vybly.io
+      "www.vybly.io", // Allow www.vybly.io
+    ],
   },
   preview: {
-    port: 3000 // Also set preview port to 3000
-  }
-})
+    port: 3000, // Also set preview port to 3000
+  },
+});
